5f22a19f362ed798894bf298a1579d743a7a5f48
George Trubachef <trubacheff_91@mail.ru>
2021-07-25 11:10:56 +0300

    popupopen

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('1 + 1');
+let buttonEdit = document.querySelector('.profile__edit-button');
+let popupUserForm = document.querySelector('.popup');
+let buttonPopupClose = document.querySelector('.popup__close');
+
+function popupOpen() {
+    popupUserForm.classList.add('popup_opened');
+}
+
+function popupClose() {
+    popupUserForm.classList.remove('popup_opened');
+}
+
+
+
+buttonEdit.addEventListener('click', popupOpen);
+buttonPopupClose.addEventListener('click', popupClose);
