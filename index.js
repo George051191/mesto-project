b8dc54caa244324d4fecc5fc4077bebfa95d4fab
George Trubachef <trubacheff_91@mail.ru>
2021-07-25 20:25:51 +0300

    popupdone

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 let buttonEdit = document.querySelector('.profile__edit-button');
 let popupUserForm = document.querySelector('.popup');
 let buttonPopupClose = document.querySelector('.popup__close');
+let userForm = document.querySelector('.popup__user-info');
+let userName = document.querySelector('.profile__name');
+let userWork = document.querySelector('.profile__work-place');
+let nameInput = document.querySelector('.popup__user-name');
+let jobInput = document.querySelector('.popup__user-work');
 
 function popupOpen() {
     popupUserForm.classList.add('popup_opened');
+    nameInput.value = userName.textContent;
+    jobInput.value = userWork.textContent;
 }
 
 function popupClose() {
     popupUserForm.classList.remove('popup_opened');
 }
 
-
-
 buttonEdit.addEventListener('click', popupOpen);
 buttonPopupClose.addEventListener('click', popupClose);
+
+function formSubmitHandler(evt) {
+
+    evt.preventDefault();
+    userName.textContent = nameInput.value;
+    userWork.textContent = jobInput.value;
+    popupClose();
+}
+userForm.addEventListener('submit', formSubmitHandler);
